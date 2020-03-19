a286c46b170d45b703fe7f9a9d810a53d34c1154
Umar Hussain <umarhussain.work@gmail.com>
2020-03-19 22:18:50 +0100

    summer semester 2020 list added. selection between semesters added.

---
+++ public/script.js
@@ -0,0 +1,110 @@
+// Code taken from internet.
+let x, i, j, selectElement, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+for (i = 0; i < x.length; i++) {
+    selectElement = x[i].getElementsByTagName("select")[0];
+    /*for each element, create a new DIV that will act as the selected item:*/
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selectElement.options[selectElement.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /*for each element, create a new DIV that will contain the option list:*/
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < selectElement.length; j++) {
+        /*for each option in the original select element,
+        create a new DIV that will act as an option item:*/
+        c = document.createElement("DIV");
+        c.innerHTML = selectElement.options[j].innerHTML;
+        c.addEventListener("click", function (e) {
+            /*when an item is clicked, update the original select box,
+            and the selected item:*/
+            let y, i, k, s, h;
+            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            h = this.parentNode.previousSibling;
+            for (i = 0; i < s.length; i++) {
+                if (s.options[i].innerHTML === this.innerHTML) {
+                    s.selectedIndex = i;
+                    h.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("same-as-selected");
+                    for (k = 0; k < y.length; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
+            }
+            h.click();
+        });
+        b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+        /*when the select box is clicked, close any other select boxes,
+        and open/close the current select box:*/
+        e.stopPropagation();
+        let semEl = document.getElementById("sem-select");
+        let sem = semEl.options[semEl.selectedIndex].value;
+        console.log(sem);
+        if (sem !== "sem") {
+            // document.getElementById(sem)
+            for (let index = 0; index < semEl.options.length; index++) {
+                const element = semEl.options[index];
+                console.log(sem, element.value);
+                if (element.value !== "sem") {
+                    if (element.value === sem) {
+                        document.getElementById(element.value).style.display = "block";
+                    } else {
+                        document.getElementById(element.value).style.display = "none";
+                    }
+                }
+            }
+        }
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
+    });
+}
+
+function closeAllSelect(elmnt) {
+    /*a function that will close all select boxes in the document,
+    except the current select box:*/
+    let x, y, i, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    for (i = 0; i < y.length; i++) {
+        if (elmnt === y[i]) {
+            arrNo.push(i)
+        } else {
+            y[i].classList.remove("select-arrow-active");
+        }
+    }
+    for (i = 0; i < x.length; i++) {
+        if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+        }
+    }
+}
+
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
+
+let semEl = document.getElementById("sem-select");
+let sem = semEl.options[semEl.selectedIndex].value;
+
+if (sem !== "sem") {
+    // document.getElementById(sem)
+    for (let index = 0; index < semEl.options.length; index++) {
+        const element = semEl.options[index];
+        // console.log(sem, element.value);
+        if (element.value !== "sem") {
+            if (element.value === sem) {
+                document.getElementById(element.value).style.display = "block";
+            } else {
+                document.getElementById(element.value).style.display = "none";
+            }
+        }
+    }
+}
